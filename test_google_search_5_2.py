d54fb3923437234a9ee0aab30a43e761bde9afdc
Aleksandr <nikiforov1709@ya.ru>
2023-04-07 17:28:25 +0500

    переименовал модуль test_google_search_5_2.py

--- test_google_search_5_2.py
+++ test_google_search_5_2.py
@@ -2,10 +2,10 @@ from selene.support.shared import browser
 from selene import be, have
 
 
-
 def test_google_search(setup_browser):
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+
 
 def test_google_search_no_result(setup_browser):
     browser.element('[name="q"]').should(be.blank).type('trdcyt4yfvfjcegkiup').press_enter()
